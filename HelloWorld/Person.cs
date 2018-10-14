fd42312cc1cedb5a214133806c3dd0e93a6bd480
Toyin Ariyo <toyinariyo66@gmail.com>
2018-10-14 20:32:13 +0100

    Add project files.

---
+++ HelloWorld/Person.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace HelloWorld
+{
+    class Person
+    {
+        String name;
+        int age;
+        String email;
+        String aLine1;
+        String aLine2;
+        String postcode;
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String _name)
+        {
+            name = _name;
+            Console.WriteLine("Hello " + _name);
+        }
+
+        public int getAge()
+        {
+            return age;
+        }
+
+        public void setAge(int _age)
+        {
+            age = _age;
+            Console.WriteLine("Age " + _age);
+        }
+
+        public String getEmail()
+        {
+            return email;
+        }
+
+        public void setEmail(String _email)
+        {
+            email = _email;
+            Console.WriteLine("Email " + _email);
+        }
+
+        public String getALine1()
+        {
+            return aLine1;
+        }
+
+        public void setALine1(String _aline1)
+        {
+            aLine1 = _aline1;
+            Console.WriteLine("Address line 1 " + _aline1);
+        }
+
+        public String getALine2()
+        {
+            return aLine2;
+        }
+
+        public void setALine2(String _aline2)
+        {
+            aLine2 = _aline2;
+            Console.WriteLine("Address line 2 " + _aline2);
+        }
+
+        public String getPostcode()
+        {
+            return postcode;
+        }
+
+        public void setPostcode(String _postcode)
+        {
+            postcode = _postcode;
+            Console.WriteLine("Postcode " + _postcode) ;
+        }
+
+        public void printDetails()
+        {
+            Console.WriteLine(name + "\n" + age + "\n" + email + "\n" + aLine1 + "\n" + aLine2 + "\n" + postcode);
+        }
+
+
+
+    }
+}
---
+++ HelloWorld/hello.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Person n = new Person();
+            Console.WriteLine("What is your name");
+            String name = null;
+            name = Console.ReadLine();
+            n.setName(name);
+            Console.WriteLine("What is your age?");
+            int age = 0;
+            age = Convert.ToInt32(Console.ReadLine());
+            n.setAge(age);
+            Console.WriteLine("What is your email?");
+            String email = null;
+            email = Console.ReadLine();
+            n.setEmail(email);
+            Console.WriteLine("What is your first address line?");
+            String addressLine1;
+            addressLine1 = null;
+            addressLine1 = Console.ReadLine();
+            n.setALine1(addressLine1);
+            Console.WriteLine("What is your second address line?");
+            String addressLine2;
+            addressLine2 = null;
+            addressLine2 = Console.ReadLine();
+            n.setALine2(addressLine2);
+            Console.WriteLine("What is your postcode?");
+            String postcode;
+            postcode = null;
+            postcode = Console.ReadLine();
+            n.setPostcode(postcode);
+            Console.WriteLine("");
+            n.printDetails();
+            //Person x = new Person();
+            //x.setName("Hello{0}" + name);
+        }
+    }
+}
